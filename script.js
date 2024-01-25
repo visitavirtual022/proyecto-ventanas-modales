ca8980eab80fc90cfe8c953413cf693e0779743b
Javier <javiercasado022@gmail.com>
2024-01-25 20:21:41 +0100

    Creamos las funciones

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+//Creamos las funciones
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
