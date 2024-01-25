d30ddd1fd2798e6b32e6aaf1849bf1f42fba19f8
Javier <javiercasado022@gmail.com>
2024-01-25 20:18:41 +0100

    Declaramos las variables

---
+++ script.js
@@ -0,0 +1,5 @@
+//Declaramos las variables
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
